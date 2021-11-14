03eea6892ed18dce299671c554606d328b86e93b
Cody Hoover <hoovercj@users.noreply.github.com>
2021-11-14 18:07:58 +0100

    Update map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -25,7 +25,9 @@ function drawVisualization() {
     ['Finland',1,'Crossed "See the Northern Lights" off my bucket list, drove a dogsled, enjoyed a true Finnish sauna complete with an ice-water plunge and rolling in the snow, and found out how delicious reindeer taste.'],
     ['Latvia',1,'Finally tried Rocky Mountain Oysters (bull testicles) while listening to locals perform Rockabilly music.'],
     ['Ireland',1,'Spent 90% of my time in pubs listening to live music and 10% driving through the incredible countryside.'],
-    ['Romania',1,'Attended a traditional Romanian wedding with incense, singing, and a reception dinner that served the final course at 3am.']
+    ['Romania',1,'Attended a traditional Romanian wedding with incense, singing, and a reception dinner that served the final course at 3am.'],
+    ['Switzerland',1,'Zurich means "too rich" in German. Or at least it looks like it.']
+    ['Croatia',1,'Shamelessly walked the Walk of Shame making bell sounds while chanting "Shame"']
   ]);
 
   var options = {
